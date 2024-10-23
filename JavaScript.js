272eea5dad345a13dc5a020b87421e07106e0a2d
LAWAL TEMITAYO OLUWADAMILARE <dammy4real201219@gmail.com>
2024-10-23 15:46:38 +0100

    My First Project

--- JavaScript.js
+++ JavaScript.js
@@ -1,6 +1,15 @@
-window.alert("I have added CSS to my HTML design to develop my web application further. The CSS is used to style the HTML elements. I continuation, I added JavaScript to the web application by adding a button that can change the paragraph content color. using onclick function to link the button from HTML to the JavaScript(JS) command. I used getElementById to target the id of the paragraph element and connected it successfully to the button. on the click of the button, the color of the paragraph content will change from the original color of blue to red. Here I have been able to discover that JavaScript is very powerful when designing a web application because it is the tool that allows the developer to add functions to the web application. In the video link that was added to the assignment. I learned JavaScript fundamentals, like variables, functions, assignments, operations symbols, and console logs. It was further explained that we could add JS to my elements in the HTML development environment and also link it to the it JS external development environment. The internal approach uses scripts opening and closing tags, while the external uses ref to link the JS to the HTML contents.  ")
+// window.alert("I have added CSS to my HTML design to develop my web application further. The CSS is used to style the HTML elements. I continuation, I added JavaScript to the web application by adding a button that can change the paragraph content color. using onclick function to link the button from HTML to the JavaScript(JS) command. I used getElementById to target the id of the paragraph element and connected it successfully to the button. on the click of the button, the color of the paragraph content will change from the original color of blue to red. Here I have been able to discover that JavaScript is very powerful when designing a web application because it is the tool that allows the developer to add functions to the web application. In the video link that was added to the assignment. I learned JavaScript fundamentals, like variables, functions, assignments, operations symbols, and console logs. It was further explained that we could add JS to my elements in the HTML development environment and also link it to the it JS external development environment. The internal approach uses scripts opening and closing tags, while the external uses ref to link the JS to the HTML contents.  ")
 
 function change(){
   document.getElementById('welcome').style.color = 'green';
 }
 
+
+
+// var age = prompt('What is your date of Birth?');
+
+// document.getElementById('dname').innerHTML = age;
+
+function color () {
+  document.getElementById('bulb').style.backgroundColor = "green"
+}
